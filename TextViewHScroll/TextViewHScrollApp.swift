bc244720e42f5b4fc2bb745b8eb1f7c081d540fe
Arnold Rozon <arnold@rozon.org>
2024-08-19 19:09:34 -0700

    Initial Commit

---
+++ TextViewHScroll/TextViewHScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextViewHScrollApp.swift
+//  TextViewHScroll
+//
+//  Created by Arnaldo Rozon on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TextViewHScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
